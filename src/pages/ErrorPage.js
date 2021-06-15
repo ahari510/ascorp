1a35c9a1208f609422602f4923ef5c97edc83d2a
ahari510 <kumarsuresh_nsitec@yahoo.com>
2021-06-16 01:32:31 +0800

    updated

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -16,7 +16,7 @@ const ErrorPage = () => {
 };
 
 const Wrapper = styled.main`
-  background: var(--clr-primary-10);
+  background: var(--clr-grey-10);
   display: flex;
   justify-content: center;
   align-items: center;
